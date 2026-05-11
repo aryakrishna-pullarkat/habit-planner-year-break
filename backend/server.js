a518f99ee824972379a7117c661616bfccee741b
aryakrishna-pullarkat <u2403071@rajagiri.edu.in>
2026-05-11 20:19:39 +0530

    fixed register logic

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
-console.log(process.env.MONGO_URI);
-
-const User = require("./models/User");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
 app.post("/api/auth/register", async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { username, email, password } = req.body;
+
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+
+    const existingUsername = await User.findOne({ username });
+
+    if (existingUsername) {
+      return res.status(400).json({
+        message: "Username already exists",
+      });
+    }
 
     const newUser = new User({
+      username,
       email,
       password,
     });
@@ -44,8 +37,4 @@ app.post("/api/auth/register", async (req, res) => {
       message: "Server error",
     });
   }
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
 });
