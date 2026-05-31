dc57a01b3ae10c129bd709fa81eb0e2d63ebd673
dixondanty <dixondanty707@gmail.com>
2026-05-31 20:37:34 +0530

    update on footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -17,17 +17,18 @@ const styles = {
     padding: "18px",
     textAlign: "center",
     zIndex: 100,
-    background: "rgba(255,255,255,0.15)",
+    background: "rgba(255, 255, 255, 0.83)",
     backdropFilter: "blur(10px)",
     boxSizing: "border-box",
   },
 
   text: {
-    color: "white",
+    color: "blue",
     fontSize: "14px",
+    fontWeight: "700",
     letterSpacing: "0.5px",
     margin: 0,
-    textShadow: "0 2px 10px rgba(0,0,0,0.3)",
+    textShadow: "0 2px 10px rgba(0, 0, 0, 0.23)",
   },
 };
 
